ecae21cbe0c7bc0ab626bbf2b26dabb61b2a9dd8
DavLac <david.lacoste2@gmail.com>
2022-10-24 00:41:43 +0800

    Commit

--- src/component/title/Title.js
+++ src/component/title/Title.js
@@ -1,43 +1,75 @@
-import React from 'react';
+import React from "react";
 import { Section } from "../element/section/Section";
 
 const summaryTitles = [
-  {title: "ES6 features", anchor: "#es6-anchor", color: "green"},
-  {title: "useState: react to a button", anchor: "#state-anchor", color: "green"},
-  {title: "Clean event listener", color: "red"},
-  {title: "Cross component state - child to parent", anchor: "#cross-anchor", color: "green"},
-  {title: "useEffect: load API call at runtime", anchor: "#effect-anchor", color: "green"},
-  {title: "Clean API call", color: "red"},
-  {title: "useState, submit form, API call with placeholder, map response with a class", anchor: "#api-anchor", color: "green"},
-  {title: "React.memo, useMemo, useCallback", anchor: "#memo-anchor", color: "orange"},
-  {title: "useReducer: manage big forms", anchor: "#form-anchor", color: "orange"},
-  {title: "Router + useHistory", anchor: "#router-anchor", color: "green"},
-  {title: "useContext", anchor: "#context-anchor", color: "green"},
-  {title: "Redux", anchor: "#redux-anchor", color: "orange"},
-  {title: "Responsive CSS: media queries, flex and grid", anchor: "#responsive-anchor", color: "green"},
-  {title: "Testing: Jest", color: "green"},
-  {title: "Testing: Cypress", color: "green"}
-]
+  { title: "ES6 features", anchor: "#es6-anchor", color: "green" },
+  {
+    title: "useState: react to a button",
+    anchor: "#state-anchor",
+    color: "green",
+  },
+  { title: "Clean event listener", color: "red" },
+  {
+    title: "Cross component state - child to parent",
+    anchor: "#cross-anchor",
+    color: "green",
+  },
+  {
+    title: "useEffect: load API call at runtime",
+    anchor: "#effect-anchor",
+    color: "green",
+  },
+  { title: "Clean API call", color: "red" },
+  {
+    title:
+      "useState, submit form, API call with placeholder, map response with a class",
+    anchor: "#api-anchor",
+    color: "green",
+  },
+  {
+    title: "React.memo, useMemo, useCallback",
+    anchor: "#memo-anchor",
+    color: "orange",
+  },
+  {
+    title: "useReducer: manage big forms",
+    anchor: "#form-anchor",
+    color: "orange",
+  },
+  { title: "Router + useHistory", anchor: "#router-anchor", color: "green" },
+  { title: "useContext", anchor: "#context-anchor", color: "green" },
+  { title: "Redux", anchor: "#redux-anchor", color: "orange" },
+  {
+    title: "Responsive CSS: media queries, flex and grid",
+    anchor: "#responsive-anchor",
+    color: "green",
+  },
+  { title: "Testing: Jest", color: "green" },
+  { title: "Testing: Cypress", color: "green" },
+];
 
 const generateSummaryTitles = () => {
-  return summaryTitles.map((e, index) => 
-      <li key={index} style={{color: e.color}}>
-        {(e.anchor) ? <a href={e.anchor}>{e.title}</a> : e.title}
-      </li>
-    )
-}
+  return summaryTitles.map((e, index) => (
+    <li key={index} style={{ color: e.color }}>
+      {e.anchor ? <a href={e.anchor}>{e.title}</a> : e.title}
+    </li>
+  ));
+};
 
 export const Title = () => {
   return (
-    <Section background='light'>
+    <Section background="light">
       <h1 className="title, h1-main">Frontend training</h1>
       <h2 class="subtitle">Summary</h2>
 
       <span>Overall training progress : 10 ok, 3 in progress on 15</span>
-      <progress class="progress is-success" value="11.5" max="15" style={{width: "400px", border: "1px black solid"}}></progress>
-      <ul>
-        {generateSummaryTitles()}
-      </ul>
+      <progress
+        class="progress is-success"
+        value="11.5"
+        max="15"
+        style={{ width: "400px", border: "1px black solid" }}
+      ></progress>
+      <ul>{generateSummaryTitles()}</ul>
     </Section>
   );
 };
--- src/component/trainings/memocallback/FetchApi.js
+++ src/component/trainings/memocallback/FetchApi.js
@@ -1,33 +1,42 @@
-
-import React, { useState, useEffect, useCallback } from 'react';
-import BoredApiGateway from './http/BoredApiGateway';
+import React, { useState, useEffect, useCallback } from "react";
+import BoredApiGateway from "./http/BoredApiGateway";
 
 const objectResponseToJsxList = (dataObject) => {
   let rows = [];
   let index = 0;
-  for(const [key, value] of Object.entries(dataObject)) {
-    rows.push(<li key={index}>{key} : {value}</li>);
+  for (const [key, value] of Object.entries(dataObject)) {
+    rows.push(
+      <li key={index}>
+        {key} : {value}
+      </li>
+    );
     index++;
   }
   return <ul>{rows}</ul>;
-}
+};
 
 const FetchApi = (props) => {
   const [activity, setActivity] = useState();
 
   const getCallBackActivity = useCallback(async () => {
-    setActivity('Loading...');
+    setActivity("Loading...");
     console.log("Loading data...");
     return await BoredApiGateway.getActivity();
   }, [activity]);
 
-  useEffect(() => getCallBackActivity()
-      .then(res => setActivity(objectResponseToJsxList(res.data)))
-      .catch(error => setActivity(JSON.stringify(error))), 
-      [getCallBackActivity]);
+  useEffect(
+    () =>
+      getCallBackActivity()
+        .then((res) => setActivity(objectResponseToJsxList(res.data)))
+        .catch((error) => setActivity(JSON.stringify(error))),
+    [getCallBackActivity]
+  );
 
   return (
-    <div>Response {props.click} : <br/>{(activity) ? <>{activity}</> : null}</div>
+    <div>
+      Response {props.click} : <br />
+      {activity ? <>{activity}</> : null}
+    </div>
   );
 };
 
